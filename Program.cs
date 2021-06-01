cd801cdd3ef64dbd1bc1c303c2b9b3aa1ca55525
Joel Gage <jrossgage@gmail.com>
2021-06-01 13:30:41 -0500

    Completed phase 4

--- Program.cs
+++ Program.cs
@@ -11,8 +11,12 @@ namespace Heist
             Team team = new Team();
             int bankDifficulty = 100;
             int teamSkill = 0;
+            int numOfRuns = 1;
+            int totalDifficulty = 0;
             UserInput();
 
+            //Amount of runs.
+            //For loop that keeps the user inputted data the same but changes heistLuck and checks the outcome each time.
             void UserInput()
             {
                 Console.Write("Enter team member name > ");
@@ -23,6 +27,8 @@ namespace Heist
                     int addSkillLevel = int.Parse(Console.ReadLine());
                     Console.Write("Enter team member courage factor (a number between 0.0 and 2.0 > ");
                     float addCourageFactor = float.Parse(Console.ReadLine());
+                    Console.WriteLine(@"
+                    ");
                     TeamMember newMember = new TeamMember(addName, addSkillLevel, addCourageFactor);
                     team.Members.Add(newMember);
                     UserInput();
@@ -34,6 +40,27 @@ namespace Heist
                     {
                         Console.WriteLine(Member.GetDescription());
                         teamSkill += Member.SkillLevel;
+                    }
+                    Console.WriteLine($"Total team skill level: {teamSkill}");
+                    Console.WriteLine($"Bank's difficulty level is {totalDifficulty}");
+                    Console.Write("How many trial runs do you want? > ");
+                    numOfRuns = int.Parse(Console.ReadLine());
+                }
+            }
+            void TrialRuns()
+            {
+                int heistLuck = new Random().Next(-10, 10);
+                totalDifficulty = bankDifficulty + heistLuck;
+
+                for (int i = 0; i <= numOfRuns; i++)
+                {
+                    if (teamSkill >= bankDifficulty)
+                    {
+                        Console.WriteLine("Successful bank heist!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failed.");
                     }
                 }
             }
